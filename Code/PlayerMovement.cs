e7c6f8230b100de2cdc2bbd7a05ee7c14d58e517
Trinabh Datta <trinabhdatta@gmail.com>
2024-02-08 13:35:57 +0530

    SinglePlayer movement script

--- Code/PlayerMovement.cs
+++ Code/PlayerMovement.cs
@@ -1,62 +1,56 @@
+using FishNet.Object;
 using UnityEngine;
+using FishNet.Connection;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float mouseSensitivity = 500f;
+    float movementSpeed = 6f;
+    public float xRotation = 0f;
 
+    [SerializeField] Camera PlayerCamera;
     private CharacterController controller;
-    
-    public float speed = 12f;
-    public float gravity = -9.8f * 2;
-    public float jumpHeight = 3f;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public Layer groundMask;
+    private float xInput;
+    private float yInput;
+    private float mouseX;
+    private float mouseY;
+    Vector3 movemenentVector;
 
-    Vector3 velocity;
-    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
-
-    bool isGrounded;
-    bool isMoving;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-    }
+       Cursor.lockState = CursorLockMode.Locked;
+       controller = GetComponent<CharacterController>();
+}
 
     void Update()
     {
-        isGrounded = Physics.Checksphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0) {
-            velocity.y = -2f;
-        }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-
-        if (lastPosition != gameObject.transform.position && isGrounded == true)
-        {
-            isMoving = true;
-        }
-
-        else {
-            isMoving = false;
-        }
-
-        lastPosition = gameObject.transform.position;
-
+        HandleMovement();
+        HandleMouseLook();
     }
+
+
+    void HandleMouseLook()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        mouseX = Mathf.Clamp(mouseX, -90f, 90f);
+
+        PlayerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void HandleMovement() 
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        movemenentVector = transform.right * xInput + transform.forward * yInput;
+        movemenentVector = movemenentVector.normalized;
+        controller.Move(movemenentVector * movementSpeed * Time.deltaTime);
+    }
+
 }
